039b82f316b9a675c7d8c8fb8fb9af5c552b2387
Andrei Miulescu <lusu777@gmail.com>
2013-12-12 13:12:15 +1100

    validations and simple for scaffolding

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :timeline
   belongs_to :user
 
+  validates_presence_of :headline, :text, :media, :caption, :thumbnail , :start_date, :end_date, :timeline
+
 end
--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -2,4 +2,6 @@ class Timeline < ActiveRecord::Base
   self.inheritance_column = :_type_disabled
   belongs_to :user
   has_many :posts, dependent: :destroy
+
+  validates_presence_of :headline, :text, :media, :caption, :thumbnail , :type
 end
