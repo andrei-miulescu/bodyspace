f8e38ac5fdb0477cb99a56ba59e60c11732aa5f8
Andrei Miulescu <lusu777@gmail.com>
2013-12-12 20:53:27 +1100

    added images in assets

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(:default, Rails.env)
 module Bodyspace
   class Application < Rails::Application
 
-    config.assets.paths << Rails.root.join("app", "assets", "fonts") 
+    config.assets.paths << Rails.root.join("app", "assets", "fonts", "images")
 
     config.assets.precompile << Proc.new { |path|
       if path =~ /\.(eot|svg|ttf|woff)\z/
