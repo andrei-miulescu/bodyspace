fd54c8ff02d1a25cbfac09472cb85aa36009a932
Andrei Miulescu <lusu777@gmail.com>
2013-12-15 15:42:38 +1100

    scope for diet supplements

--- app/models/supplement.rb
+++ app/models/supplement.rb
@@ -2,5 +2,5 @@ class Supplement < ActiveRecord::Base
   has_many :nutritional_items, :dependent => :delete_all
   belongs_to :diet
   validates_presence_of :title, :image_url, :url, :diet
-  validates_uniqueness_of :title
+  validates_uniqueness_of :title, :scope => :diet
 end
