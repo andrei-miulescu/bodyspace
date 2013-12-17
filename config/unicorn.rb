b0215e5b23a1b1d808d1d37e5c69d45a5ee1c3ec
Andrei Miulescu <lusu777@gmail.com>
2013-12-17 14:42:26 +1100

    redis startup before unicorn stuff

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,6 +9,10 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
+  redis_uri = YAML.load_file("#{Rails.root}/config/redis.yml")[ENV['RAILS_ENV']]
+  uri = URI.parse(redis_uri)
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
   @sidekiq_pid ||= spawn('bundle exec sidekiq -e production -C config/sidekiq.yml')
 
   defined?(ActiveRecord::Base) and
