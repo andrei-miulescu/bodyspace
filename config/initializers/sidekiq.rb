faf94f532e0eaf9a1c39799cd6045ebb0a7be19b
Andrei Miulescu <lusu777@gmail.com>
2013-12-17 14:50:08 +1100

    heroku hack

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 require 'sidekiq'
 
 Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
+  config.redis = { :size => 1, :url => 'redis://redis-app20223279:JRr0tLrDk1S7BBaw@pub-redis-16622.us-east-1-2.3.ec2.garantiadata.com:16622', :namespace => 'mynamespace' }
 end
 
 Sidekiq.configure_server do |config|
@@ -9,5 +9,5 @@ Sidekiq.configure_server do |config|
   # concurrency value so you do not need to
   # specify this. For this demo I do
   # show it to understand the numbers
-  config.redis = { :size => 9 }
+  config.redis = { :size => 9, :url => 'redis://redis-app20223279:JRr0tLrDk1S7BBaw@pub-redis-16622.us-east-1-2.3.ec2.garantiadata.com:16622', :namespace => 'mynamespace' }
 end
