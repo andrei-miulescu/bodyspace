59ec5403af1f3584d735be302d74041ed2499d51
Andrei Miulescu <lusu777@gmail.com>
2013-12-17 15:37:30 +1100

    reverted figaro stuff

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
 
+redis_uri = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
 uri = URI.parse(Figaro.env.redis_url)
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
