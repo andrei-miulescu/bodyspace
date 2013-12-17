7a1d4e1f31965f58f35841be0fe7a7b9cd321373
Andrei Miulescu <lusu777@gmail.com>
2013-12-17 14:26:03 +1100

    rak env

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 
-redis_uri = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
+redis_uri = YAML.load_file("#{Rails.root}/config/redis.yml")[ENV['RAILS_ENV']]
 uri = URI.parse(redis_uri)
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
